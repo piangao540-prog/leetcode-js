9d13ceb7f669999d59bfe2d5de0be42538ca4de1
wc <iii@qq.com>
2026-07-05 21:59:16 +0800

    feat: 两数之和 + 反转链表

---
+++ 1-两数之和/index.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+    const map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        const remain = target - nums[i]
+        if (map.has(remain)) {
+            return [map.get(remain), i]
+        }
+        map.set(nums[i], i)
+    }
+}
